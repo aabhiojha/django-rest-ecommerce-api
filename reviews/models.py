1a294a0f583d8c468c6bf70f8e6af08590d9de10
Abhishek Ojha <aojha6822@gmail.com>
2025-10-28 22:39:35 -0700

    reviews initialization

---
+++ reviews/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from products.models import Product
+from users.models import User
+
+class Review(models.Model):
+    RATING_CHOICES = [
+        (1,"ONE")
+        (2,"TWO")
+        (3,"THREE")
+        (4,"FOUR")
+        (5,"FIVE")
+    ]
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="reviews")
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.CharField(choices=RATING_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"review of {self.product} by {self.user}"
