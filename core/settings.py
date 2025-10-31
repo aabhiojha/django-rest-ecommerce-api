90470e7fce9128854ce0830eff0ef4dd5673c05f
Abhishek Ojha <aojha6822@gmail.com>
2025-10-30 23:47:29 -0700

    chore: extend JWT token lifetimes

--- core/settings.py
+++ core/settings.py
@@ -174,8 +174,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=3),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=20),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
     "ALGORITHM": "HS256",
